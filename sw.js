bb7dd1336ae43eb266845564742a8dc4ff0a8d4a
Alejandro <alejandrohurtado0987@gmail.com>
2026-08-11 13:41:02 -0500

    Tablero de salidas: pizarra terminal LED + PWA + caja + favoritos + impresion termica + teclado + backup

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = 'rutacc-v1';
+const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then(c => c.addAll(CORE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      const net = fetch(e.request)
+        .then(res => {
+          if (res && res.ok && res.type === 'basic') {
+            const clone = res.clone();
+            caches.open(CACHE).then(c => c.put(e.request, clone));
+          }
+          return res;
+        })
+        .catch(() => hit);
+      return hit || net;
+    })
+  );
+});
