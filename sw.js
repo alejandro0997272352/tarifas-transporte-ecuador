1cfd53c3078ca3c64a2d6262316debbc4a64f789
Alejandro <alejandrohurtado0987@gmail.com>
2026-08-12 00:11:53 -0500

    v18: espaciado consistente y aire en toda la interfaz (contenido 34px, paneles 30px/24px, kpis 28-32px, listas y caja con mas separacion)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'rutacc-v17';
+const CACHE = 'rutacc-v18';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 const SUPA_HOST = 'uqpbnhxuiekjdadzybon.supabase.co';
 
